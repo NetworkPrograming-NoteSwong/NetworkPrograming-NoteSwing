d92009d17027266fee95aa5223d32b86e00ba89e
Kimsejin-56 <saj5877@naver.com>
2025-11-19 16:01:10 +0900

    구조도 관련

---
+++ src/global/enums/Mode.java
@@ -0,0 +1,12 @@
+package global.enums;
+
+public enum Mode {
+    TEXT,           // 텍스트 수정
+    IMAGE,          // 이미지 전송
+    CURSOR,         // 커서 이동
+    BLOCK_CREATE,   // 새 블록 생성
+    BLOCK_DELETE,   // 블록 삭제
+    JOIN,           // 접속
+    LEAVE           // 퇴장
+}
+
---
+++ src/global/object/EditMessage.java
@@ -0,0 +1,22 @@
+package global.object;
+
+import global.enums.Mode;
+
+import java.io.Serializable;
+
+public class EditMessage implements Serializable {
+    public Mode mode;   // ENUM!
+    public String userId;
+    public String text;
+    public byte[] payload;
+    public int blockId;
+    public int offset;
+    public int length;
+
+    public EditMessage(Mode mode, String userId, String text) {
+        this.mode = mode;
+        this.userId = userId;
+        this.text = text;
+    }
+}
+
